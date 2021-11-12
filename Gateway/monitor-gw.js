cc265c7e8c20f2b8aecfaa2ed054177235b97168
vgrau98 <victor.grau93@gmail.com>
2021-11-12 20:00:38 +0100

    commit gateway monitor, commented

--- Gateway/monitor-gw.js
+++ Gateway/monitor-gw.js
@@ -7,23 +7,17 @@ var fs = require('fs');
 
 ///////////////Parameters/////////////////
 //CSE Params
-var mnURI = "http://localhost:8282"; //port du mn sur la gateway
+var mnURI = "http://localhost:8282"; //mn port on the gateway
 var inURI = "http://localhost:8080"; 
 var sensorIP="";
 
-//AE params
-var aeId = "Cae-monitor1";
-var monitorIp = "localhost";
-var monitorPort = 1400;
 
-var lumMean = 0; // mean for luminosity on 1 hours
-var humMean = 0;
-var waterFlow=0;
-var heightMean=0;
-var temperatureMean=0;
+//To change regarding the configuration. IP and port on the gateway, the raspberry. 
+var monitorIp="localhost";
+var monitorPort=1400;
 
 //Mean values of sensors : 
-//Luminosity, humidity, water consoumption, height, temperature
+//Luminosity, humidity, water consumption, height, temperature
 var sensorsMean=[0, 0, 0, 0, 0];
 
 //To compute mean values
@@ -31,11 +25,9 @@ var sensorsCounter = [0, 0, 0, 0, 0];
 
 var curSensor = -1;
 
-var  i = 0;
-
 var date = new Date();
 var hour = date.getHours();
-var prevHour = 0; //previous period in the day
+var prevHour = 0; //previous period in the day. Here periods are hours
 
 var value=0;
 
@@ -47,13 +39,16 @@ var end=0;
 //////////////////////////////////////////
 
 
-
+//Create AE SENSOR_PLATFORM in the IN node (server). This is to this AE that data collected from ESP32 will be transmitted
 initSensorPlatform();
 
 
 /////////////////////////////////////////
 
 app.use(bodyParser.json());
+
+//To receive request from the MN-CSE as soon as a new content instance is published on the mn-node. Data come from several sensors : lumonisity, humidity, temperature etc... 
+//We have one request for one sensor. For each request we have to know sensor type. 
 app.listen(monitorPort, function () {
 	console.log("Monitor listening on: "+monitorIp+":"+monitorPort);
 });
@@ -61,11 +56,16 @@ app.listen(monitorPort, function () {
 app.post('/', function (req, res) {
 	console.log("\n◀◀◀◀◀")
 	console.log(req.body);
+
+
+	//Extract the request content
 	console.log(req.body["m2m:sgn"]["m2m:nev"]["m2m:rep"]["m2m:cin"]["con"]);
 	var content = req.body["m2m:sgn"]["m2m:nev"]["m2m:rep"]["m2m:cin"]["con"];
 
 	start=content.indexOf("sensor");
 	end=content.indexOf(",", start);
+
+	//extract the type of sensor
 	sensor_type=content.substring(start+9, end);
 
 
@@ -95,77 +95,48 @@ app.post('/', function (req, res) {
 	date=new Date();
 	hour = date.getHours();
 
-	//content = "{value : "+content+", unit : lux , location : home , period : "+ hour+" , day : "+date.getDate()+"-"+date.getMonth()+"-"+date.getFullYear()+"}"
-	//createContenInstance(content, "LUMINOSITY_SENSOR", "DATA");
 
-
-
-	//After one hour, we compute each mean and send them to IN
-	//if (hour!==prevHour && sensorsMean!==0) {
-
+	// If one hour has passed
 	if (hour!==prevHour && sensorsMean.reduce(function(pv, cv) { return pv + parseInt(cv); }, 0)!==0){
+
+
 
 		sensorsMean[curSensor]=sensorsMean[curSensor]+parseInt(value);
 		sensorsCounter[curSensor]=sensorsCounter[curSensor]+1;
 
-		//lumMean=lumMean+parseInt(value);
-		//i=i+1;
-		//lumMean=lumMean/i;
 
 		for(let i=0; i<5; i++){
 
+			//We compute the mean of each value (luminosity, temperature etc...) before send them to the IN. We think that the mean over one hour is releavant, but we can add other features like 
+			//the max or/and min value over one hour or real time data. We don't want to clog up the network and reduce device consumption. That's what we want to send as 
+			//little data as possible between the gateway and the server while keeping a maximum of information mon crop's environment. 
 			sensorsMean[i]=sensorsMean[i]/sensorsCounter[i];
 		}
         
-        //Multiply water_flow by its time unit to have water consumption over one hour
+        //We concatenate means of data sensors over one hour to send them to the IN node
         content = "{luminosity : "+sensorsMean[0]+", humidity : "+sensorsMean[1]+", water_consumption : "+sensorsMean[2]+", height : "+sensorsMean[3]+", temperature : "+sensorsMean[4]+", period : "+ prevHour+" ,day : "+date.getDate()+"-"+date.getMonth()+"-"+date.getFullYear()+ "}";
-		//content = "{value : "+lumMean+"unit : lux, location : home, period : "+ hour+" day : "+date.getDate()+"-"+date.getMonth()+"-"+date.getFullYear()+"}";
 
+		
+        //Content instance is created on the IN node
 		createContenInstance(content, "SENSOR_PLATFORM", "DATA");
 		
 		sensorsMean.forEach((element, index) => {sensorsMean[index]=0;});
 		sensorsCounter.forEach((element, index) => {sensorsCounter[index]=0;});
-		//lumMean=0;
-		//i=0;
+		
 
 
 
 
 	}else{
 
+		//If one hour hasn't passed we add current value to the previous and increment the counter to compute the mean later (every one hour)
 		sensorsMean[curSensor]=sensorsMean[curSensor]+parseInt(value);
 		sensorsCounter[curSensor]=sensorsCounter[curSensor]+1;
-
-		//lumMean=lumMean+parseInt(value);
-		//i=i+1;
 
 
 	}
 
-	console.log(sensorsMean[0]);
-	//console.log(prevHour);
 	prevHour=hour;
-
-
-
-	
-	/*s.writeFile("test.txt", content,{ flag: 'a+' }, function(err) {
-    if (err) {
-        console.log(err);
-    	}
-	});*/
-
-	/*if(content>300 && ledON ){
-		console.log("High luminosity => Switch led OFF");
-		createContenInstance("OFF");
-		ledON=false;
-	}else if(content<=300 && !ledON){
-		console.log("Low luminosity => Switch led ON");
-		createContenInstance("ON")
-		ledON=true;
-	}else{
-		console.log("Nothing to do");
-	}*/
 	res.sendStatus(200);
 });
 
@@ -348,7 +319,6 @@ function initSensorPlatform(){
 			"rn":"SENSOR_PLATFORM",			
 			"rr":"true",
 			"api":"app.company.com"
-			//"poa":"http://localhost:1400/"
 		}
 	};
 
@@ -366,6 +336,7 @@ function initSensorPlatform(){
 		json: representation
 	};
 
+	//Create the AE SENSOR_PLATFORM on the IN node (server)
 	request(options, function (error, response, body) {
 		console.log("◀◀◀◀◀");
 		if(error){
@@ -373,56 +344,54 @@ function initSensorPlatform(){
 		}else{
 			console.log(response.statusCode);
 			console.log(body);
-			createCNT("SENSOR_PLATFORM","DESCRIPTOR","{location : home, sensors : [luminosity (lux), temperature (°C), humidity (%), water consumption (L)]}, height (cm)");
+			createCNT("SENSOR_PLATFORM","DESCRIPTOR","{location : [latidude, longitude], crops/plants : cotton, sensors : [luminosity (lux), temperature (°C), humidity (%), water consumption (L),  height (cm)]}");
 			createCNT("SENSOR_PLATFORM","DATA");
-
-			//createSubscription();
 		}
 	});
 
 }
 
-function initWaterActuator(){
+//function initWaterActuator(){
 
 
-console.log("\n▶▶▶▶▶");
-	var method = "POST";
-	var uri= "http://localhost:8282/~/mn-cse/mn-name";
-	var resourceType=2;
-	var requestId = "123456";
-	var representation = {
-		"m2m:ae":{
-			"rn":"WATER_MANAGEMENT_GW",			
-			"rr":"true",
-			"poa":"http://"+sensorIP+":9999/",
-			"api":"api"
-		}
-	};
+// console.log("\n▶▶▶▶▶");
+// 	var method = "POST";
+// 	var uri= "http://localhost:8282/~/mn-cse/mn-name";
+// 	var resourceType=2;
+// 	var requestId = "123456";
+// 	var representation = {
+// 		"m2m:ae":{
+// 			"rn":"WATER_MANAGEMENT_GW",			
+// 			"rr":"true",
+// 			"poa":"http://"+sensorIP+":9999/",
+// 			"api":"api"
+// 		}
+// 	};
 
-	console.log(method+" "+uri);
-	console.log(representation);
+// 	console.log(method+" "+uri);
+// 	console.log(representation);
 
-	var options = {
-		uri: uri,
-		method: method,
-		headers: {
-			"X-M2M-Origin": "admin:admin",
-			"X-M2M-RI": requestId,
-			"Content-Type": "application/json;ty="+resourceType
-		},
-		json: representation
-	};
+// 	var options = {
+// 		uri: uri,
+// 		method: method,
+// 		headers: {
+// 			"X-M2M-Origin": "admin:admin",
+// 			"X-M2M-RI": requestId,
+// 			"Content-Type": "application/json;ty="+resourceType
+// 		},
+// 		json: representation
+// 	};
 
-	request(options, function (error, response, body) {
-		console.log("◀◀◀◀◀");
-		if(error){
-			console.log(error);
-		}else{
-			console.log(response.statusCode);
-			console.log(body);
-			createSubscription("WATER_ACTUATOR", "DATA");
-		}
-	});
+// 	request(options, function (error, response, body) {
+// 		console.log("◀◀◀◀◀");
+// 		if(error){
+// 			console.log(error);
+// 		}else{
+// 			console.log(response.statusCode);
+// 			console.log(body);
+// 			createSubscription("WATER_ACTUATOR", "DATA");
+// 		}
+// 	});
 
 	
-}
+// }
